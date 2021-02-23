0b828e0ddd65efa66cfd70cad06f51a82eee996d
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-02-23 18:08:53 -0300

    Segundo dia concluido

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/31699929?s=460&u=a662956c8d7b6ff94e424b7504e44b7973ac83a7&v=4" alt="Joao Vitor"/>
+            <div>
+                <strong>João Vitor Mineiro</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p> 
+            </div>
+        </div>
+    );
+} 
